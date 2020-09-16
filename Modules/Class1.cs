a80c4755ade1d69ef7e963d314bed04830e67bc4
Reisan03 <yossi.channel0000@gmail.com>
2020-09-16 13:13:43 +0900

    initial Commit

---
+++ Modules/Class1.cs
@@ -0,0 +1,27 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RezeroDiscord.Modules
+{
+    public interface Main : IDisposable
+    {
+        public class one : ModuleBase<SocketCommandContext>
+        {
+            public static DiscordSocketClient client;
+            public static BaseSocketClient socket;
+            
+            [Command("ping")]
+            public async Task Echo()
+            {
+                var ping = Context.Client.Latency;
+                await ReplyAsync(ping.ToString());
+            }
+        }
+    }
+}
